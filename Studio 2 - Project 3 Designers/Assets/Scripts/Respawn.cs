d311540663769d795dba9b1b63bf3cefa4765436
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2023-12-16 22:35:34 +0400

    Respawn Script

---
+++ Studio 2 - Project 3 Designers/Assets/Scripts/Respawn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    Vector2 startPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Restart();
+    }
+
+    void Restart()
+    {
+        transform.position = startPos;
+    }
+}
